f6489ce14720e0c680c89081c52ee78a7e9dec20
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-21 03:13:06 +0000

    removed all double quote usage

--- day_15/coffee_machine.py
+++ day_15/coffee_machine.py
@@ -1,37 +1,48 @@
 #!/usr/bin/evn python3
 
 MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
+    'espresso': {
+        'ingredients': {
+            'water': 50,
+            'coffee': 18,
         },
-        "cost": 1.5,
+        'cost': 1.5,
     },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
+    'latte': {
+        'ingredients': {
+            'water': 200,
+            'milk': 150,
+            'coffee': 24,
         },
-        "cost": 2.5,
+        'cost': 2.5,
     },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
+    'cappuccino': {
+        'ingredients': {
+            'water': 250,
+            'milk': 100,
+            'coffee': 24,
         },
-        "cost": 3.0,
+        'cost': 3.0,
     }
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    'water': 300,
+    'milk': 200,
+    'coffee': 100,
+
 }
 
 
+def report_resources():
+    for value in resources:
+        print(f'{value}:')
+
+
 if __name__ == '__main__':
-    pass
+    while True:
+        command = input('What would you like? (espresso/latte/cappuccino: ')
+        if command == 'off':
+            break
+        elif command == 'report':
+            report_resources()
