fa89b00bd0daa8ff034e3b812968ccc3c4ce3b8e
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-25 01:44:07 +0000

    refactored object names

--- day_16/main.py
+++ day_16/main.py
@@ -4,17 +4,17 @@ from money_machine import MoneyMachine
 
 if __name__ == '__main__':
     menu = Menu()
-    coffee_maker_sys = CoffeeMaker()
-    money_sys = MoneyMachine()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
     while True:
         cmd = input(f'What would you like? ({menu.get_items()[:-1]}) ')
         if cmd == 'off':
             break
         elif cmd == 'report':
-            coffee_maker_sys.report()
-            money_sys.report()
+            coffee_maker.report()
+            money_machine.report()
         elif cmd == 'latte' or 'espresso' or 'cappuccino':
             drink = menu.find_drink(cmd)
-            if coffee_maker_sys.is_resource_sufficient(drink):
-                money_sys.make_payment(drink.cost)
-                coffee_maker_sys.make_coffee(drink)
+            if coffee_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
