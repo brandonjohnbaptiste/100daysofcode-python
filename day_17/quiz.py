41e175079fc365dc752acfdc41e021638ac53dc3
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-26 23:46:17 +0000

    updated quiz to use Trivia DB questions

--- day_17/quiz.py
+++ day_17/quiz.py
@@ -4,7 +4,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = [Question(question['text'], question['answer']) for question in question_data]
+question_bank = [Question(question['question'], question['correct_answer']) for question in question_data]
 quiz = QuizBrain(question_bank)
 
 if __name__ == '__main__':
