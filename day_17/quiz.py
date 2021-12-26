e8924231b2e119ffb1503c922403d8e05b6761b7
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-26 23:06:43 +0000

    added question class and quizbrain class

---
+++ day_17/quiz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from question_model import Question
+from data import question_data
+
+question_bank = [Question(question['text'], question['answer']) for question in question_data]
+
