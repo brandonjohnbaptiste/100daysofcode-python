c9efd4e7448e5ffaeaa41cac5d9d476938ee0394
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-28 01:36:40 +0000

    dot painting gneration using turtle module

--- day_18/hirst.py
+++ day_18/hirst.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
 
 from colorgram import extract
+from turtle import Turtle, Screen, colormode
+from random import choice
 
 colors_objects = extract('hirst.jpeg', 16)
 colors = [(color.rgb.r, color.rgb.b, color.rgb.b) for color in colors_objects]
-print(colors)
+dot_size = 20
+colormode(255)
+
+initial_x = -250
+initial_y = -200
+
+turtle = Turtle()
+turtle.penup()
+turtle.setposition(initial_x, initial_y)
+
+for y in range(10):
+    print(f'Printing row {y}')
+    turtle.sety(initial_y + (y * 50))
+    turtle.setx(initial_x)
+    for x in range(10):
+        turtle.pendown()
+        turtle.dot(dot_size, choice(colors))
+        turtle.penup()
+        turtle.setx(turtle.xcor() + 50)
+
+screen = Screen()
+screen.exitonclick()
