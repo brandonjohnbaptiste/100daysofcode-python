e3a7dbdc8edd2cd846cd13dbbc71c5c43f94d7d4
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-28 00:32:38 +0000

    drawing different sided shapes with random colours

--- day_18/painting.py
+++ day_18/painting.py
@@ -1,16 +1,24 @@
 #!/usr/bin/evn python3
 
 from turtle import Turtle, Screen
+from random import choice
 
 timmy = Turtle()
+timmy.home()
 timmy.shape('turtle')
 timmy.color('cornflower blue')
 
-for n in range(10):
-    timmy.pendown()
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
+colors = ['red', 'black', 'blue', 'yellow', 'pink', 'green', 'purple', 'brown', 'orange']
+
+
+shape_side_nums = [n for n in range(4,11)]
+
+for shape in shape_side_nums:
+    timmy.color(choice(colors))
+    for n in range(shape):
+        timmy.forward(100)
+        timmy.right(360 / shape)
+
 
 
 screen = Screen()
