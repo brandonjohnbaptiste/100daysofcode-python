a16f7fae63ea32f016986e813179c3fd07b5b302
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-28 00:34:58 +0000

    updated so that it draws a triangle first

--- day_18/painting.py
+++ day_18/painting.py
@@ -11,15 +11,13 @@ timmy.color('cornflower blue')
 colors = ['red', 'black', 'blue', 'yellow', 'pink', 'green', 'purple', 'brown', 'orange']
 
 
-shape_side_nums = [n for n in range(4,11)]
+shape_side_nums = [n for n in range(3, 11)]
 
 for shape in shape_side_nums:
     timmy.color(choice(colors))
     for n in range(shape):
         timmy.forward(100)
         timmy.right(360 / shape)
-
-
 
 screen = Screen()
 screen.exitonclick()
