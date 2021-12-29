3f69a68d1f09812a49dc1085e408a1858f21f63a
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-29 23:13:32 +0000

    first etch-a-sketch draft using keys for movement and a clear function

--- day_19/etch_a_sketch.py
+++ day_19/etch_a_sketch.py
@@ -1,15 +1,37 @@
 #!/usr/bin/evn python3
 
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, clearscreen
 
 turtle = Turtle()
 screen = Screen()
+
 
 def move_forward():
     turtle.forward(10)
 
 
+def move_backward():
+    turtle.backward(10)
+
+
+def move_left():
+    turtle.left(10)
+
+
+def move_right():
+    turtle.right(10)
+
+
+def clear_screen():
+    turtle.clear()
+
+
 if __name__ == '__main__':
     screen.listen()
-    screen.onkey(key='space',fun=move_forward)
+    screen.onkey(key='w', fun=move_forward)
+    screen.onkey(key='s', fun=move_backward)
+    screen.onkey(key='a', fun=move_left)
+    screen.onkey(key='d', fun=move_right)
+    screen.onkey(key='c', fun=clear_screen)
+
     screen.exitonclick()
