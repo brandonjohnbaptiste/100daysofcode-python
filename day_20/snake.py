79976929698bfd9a2ff4ece6cb522543d4bc1606
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-30 22:54:26 +0000

    added 3 turtles into a initial posistion

--- day_20/snake.py
+++ day_20/snake.py
@@ -10,4 +10,19 @@ screen.setup(width=screen_width, height=screen_height)
 screen.bgcolor('black')
 screen.title('Snake')
 
+
+def create_turtle():
+    t = Turtle(shape='square')
+    t.color('white')
+    return t
+
+
+turtles = [create_turtle() for i in range(3)]
+init_x = 0
+init_y = 0
+offset = 20
+
+for turtle in turtles:
+    turtle.setpos(x=(init_x - (offset * turtles.index(turtle))), y=init_y)
+
 screen.exitonclick()
