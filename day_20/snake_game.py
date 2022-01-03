6c62c1a73581bba22e96c1ab27eea2fdbb6119c7
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-03 20:36:57 +0000

    updated game logic to use snake class

--- day_20/snake_game.py
+++ day_20/snake_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from turtle import Turtle, Screen
+from snake import Snake
 from time import sleep
 
 screen_height = 600
@@ -12,21 +13,7 @@ screen.bgcolor('black')
 screen.title('Snake')
 screen.tracer(0)
 
-
-def create_body_part():
-    t = Turtle(shape='square')
-    t.color('white')
-    t.penup()
-    return t
-
-
-snake_body = [create_body_part() for i in range(3)]
-init_x = 0
-init_y = 0
-offset = 20
-
-for part in snake_body:
-    part.setpos(x=(init_x - (offset * snake_body.index(part))), y=init_y)
+snake = Snake()
 
 if __name__ == '__main__':
     game_running = True
@@ -35,12 +22,9 @@ if __name__ == '__main__':
         screen.update()
         sleep(0.1)
 
-        for part_index in range(len(snake_body) - 1, 0, -1):
-            new_x = snake_body[part_index - 1].xcor()
-            new_y = snake_body[part_index - 1].ycor()
-            snake_body[part_index].goto(x=new_x, y=new_y)
+        snake.move()
 
-        snake_body[0].forward(20)
+
 
 
 
