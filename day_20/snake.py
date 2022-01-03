e27827bfa6de67b4b67e627eafbc1637e2e4216b
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-03 20:47:05 +0000

    added constants for maintaibility

--- day_20/snake.py
+++ day_20/snake.py
@@ -1,5 +1,9 @@
 #!/usr/bin/evn3 python3
 
+MOVE_DISTANCE = 20
+OFFSET = 20
+
+
 def create_body_part():
     from turtle import Turtle
     t = Turtle(shape='square')
@@ -14,10 +18,9 @@ class Snake:
 
         init_x = 0
         init_y = 0
-        offset = 20
 
         for part in self.snake_body:
-            part.setpos(x=(init_x - (offset * self.snake_body.index(part))), y=init_y)
+            part.setpos(x=(init_x - (OFFSET * self.snake_body.index(part))), y=init_y)
 
     def move(self):
         for part_index in range(len(self.snake_body) - 1, 0, -1):
@@ -25,4 +28,4 @@ class Snake:
             new_y = self.snake_body[part_index - 1].ycor()
             self.snake_body[part_index].goto(x=new_x, y=new_y)
 
-        self.snake_body[0].forward(20)
+        self.snake_body[0].forward(MOVE_DISTANCE)
