b8804910b526ae6bd584d0bcc937351c166b5165
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-04 23:22:26 +0000

    updated text size and repositioned scoreboard

--- day_20/score.py
+++ day_20/score.py
@@ -8,10 +8,13 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color('white')
+        self.sety(270)
+        self.hideturtle()
 
     def update_score(self):
         self.score += 1
 
     def display_score(self):
         self.clear()
-        self.write(f'Score: {self.score}', align='center')
+        self.write(f'Score: {self.score}', align='center', font=('Arial', 20, 'normal'))
