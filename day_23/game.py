519acaeb410b63e9b0bf2a9d8cdc507926958278
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-12 02:10:24 +0000

    added scoreboard to screen

--- day_23/game.py
+++ day_23/game.py
@@ -4,6 +4,7 @@ from turtle import Screen
 from time import sleep
 
 from player import Player
+from scoreboard import Scoreboard
 
 WIDTH = 600
 HEIGHT = 600
@@ -14,9 +15,13 @@ screen.title('Turtle Crossing')
 screen.tracer(0)
 
 player = Player()
+scoreboard = Scoreboard()
 
 if __name__ == '__main__':
     print('Starting program....')
+
+    screen.listen()
+    screen.onkeypress(player.move, 'Up')
 
     running = True
     while running:
