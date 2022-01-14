58f6e5b433812be929dcfba2f4334fe15a1d826d
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-14 15:01:38 +0000

    added saving high score to a text file

--- day_20/score.py
+++ day_20/score.py
@@ -6,12 +6,23 @@ FONT = ('Arial', 24, 'normal')
 ALIGN = 'center'
 
 
+def grab_data():
+    with open('data.txt') as data_file:
+        score = data_file.read()
+    return int(score)
+
+
+def save_data(high_score):
+    with open('data.txt', 'w') as data_file:
+        data_file.write(high_score)
+
+
 class Score(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = grab_data()
         self.color('white')
         self.penup()
         self.sety(270)
@@ -19,6 +30,7 @@ class Score(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            save_data(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.display_score()
