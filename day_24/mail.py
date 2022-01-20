99c7d8c46f18fb62fc9aa02aeb57d3e924663d1a
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-01-20 20:38:27 +0000

    added reading names and generating letters

--- day_24/mail.py
+++ day_24/mail.py
@@ -4,9 +4,13 @@ with open('input/letters/starting_letter.txt') as file:
     data = file.read()
     left_index = data.index('[') + 1
     right_index = data.index(']')
+    letter_starter = data.replace('[', '').replace(']', '')
 
     with open('input/names/invited_names.txt') as names:
         name_lst = [name.strip() for name in names]
         print(name_lst)
 
-
+    for name in name_lst:
+        letter = letter_starter.replace(data[left_index:right_index], name)
+        with open(f'output/letter_for_{name}', 'w') as final_letter:
+            final_letter.write(letter)
