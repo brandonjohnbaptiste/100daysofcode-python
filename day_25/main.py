124e2c417fdad1417449f7eb9cfc52bcb49a39f6
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-02-07 12:08:27 +0000

    using series class to find average tempeature from csv data

--- day_25/main.py
+++ day_25/main.py
@@ -2,10 +2,15 @@
 
 from pandas import read_csv
 
+
+def avg(lst):
+    return sum(lst) / len(lst)
+
+
 if __name__ == '__main__':
     data = read_csv('weather_data.csv')
-    data_dict = data.to_dict()
-    print(data_dict)
 
     temp = data['temp'].to_list()
-    print(temp)
+    avg_temp = avg(temp)
+    print(f'The average temperature was: {avg_temp:.2f}')
+
