2acc603d4d3b68b182c05a08c08dc202c92a1da9
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-02-07 12:30:47 +0000

    extracting rows from csv

--- day_25/main.py
+++ day_25/main.py
@@ -7,14 +7,23 @@ def avg(lst):
     return sum(lst) / len(lst)
 
 
+def celsius_to_fahrenheit(value):
+    return ((9/5) * value) + 32
+
+
 if __name__ == '__main__':
     data = read_csv('weather_data.csv')
 
-    temp = data['temp']
+    temp = data.temp
     avg_temp = temp.mean()
     max_temp = temp.max()
     print(f'The average temperature was: {avg_temp:.2f}')
     print(f'The maximum temperature was: {max_temp}')
+
+    monday = data[data.day == 'Monday']
+    monday_temp = int(monday.temp)
+    monday_temp_converted = celsius_to_fahrenheit(monday_temp)
+    print(f'The temperature on Monday was: {monday_temp_converted}F')
 
 
 
