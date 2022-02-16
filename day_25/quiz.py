6eecd8ac213b74c9e60e97fef51abd488cc66a94
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-02-16 22:30:03 +0000

    added looping for user input and checking for correct answer

--- day_25/quiz.py
+++ day_25/quiz.py
@@ -2,17 +2,29 @@
 from turtle import Screen, shape
 from pandas import read_csv
 
+WIDTH = 725
+HEIGHT = 491
+
 if __name__ == '__main__':
     data = read_csv('50_states.csv')
     states = data['state']
+
     screen = Screen()
     screen.title('U.S. States Quiz')
-    screen.setup(725, 491)
+    screen.setup(WIDTH, HEIGHT)
+
+    correct_guesses = []
+    guessing = True
 
     image = 'blank_states_img.gif'
     screen.addshape(image)
     shape(image)
 
-    print(states)
-    ans = screen.textinput(title='Guess the state', prompt='Enter the state\'s name')
+    while guessing:
+        ans = screen.textinput(title=f'{len(correct_guesses)}/50 States Correct', prompt='Enter the state\'s name')
+
+        for state in states:
+            if ans.capitalize() == state:
+                correct_guesses.append(ans)
+
     screen.exitonclick()
