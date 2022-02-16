e662f9cdf4fb393067dd4ddf0fbf7875e943bbb7
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-02-16 21:44:13 +0000

    grabbing squirell fur colour from csv and adding it to a dictionary

--- day_25/main.py
+++ day_25/main.py
@@ -2,28 +2,22 @@
 
 from pandas import read_csv
 
-
-def avg(lst):
-    return sum(lst) / len(lst)
-
-
-def celsius_to_fahrenheit(value):
-    return ((9/5) * value) + 32
-
-
 if __name__ == '__main__':
-    data = read_csv('weather_data.csv')
+    data = read_csv('Squirrel_Census.csv')
 
-    temp = data.temp
-    avg_temp = temp.mean()
-    max_temp = temp.max()
-    print(f'The average temperature was: {avg_temp:.2f}')
-    print(f'The maximum temperature was: {max_temp}')
+    colours = data['Primary Fur Color']
 
-    monday = data[data.day == 'Monday']
-    monday_temp = int(monday.temp)
-    monday_temp_converted = celsius_to_fahrenheit(monday_temp)
-    print(f'The temperature on Monday was: {monday_temp_converted}F')
+    fur_colours = {
+        'Black': 0,
+        'Gray': 0,
+        'Cinnamon': 0
+    }
+
+    for colour in colours:
+        if colour in fur_colours:
+            fur_colours[colour] += 1
+
+    print(fur_colours)
 
 
 
