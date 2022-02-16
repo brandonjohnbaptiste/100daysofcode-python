959f8f51666b1c2243abc0285e26ffeb83fd730b
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-02-16 22:15:55 +0000

    added user input and updated screen size to copy the image size

--- day_25/quiz.py
+++ day_25/quiz.py
@@ -1,14 +1,18 @@
 #!/usr/bin/evn/python3
-import turtle
-from turtle import Screen
+from turtle import Screen, shape
+from pandas import read_csv
 
 if __name__ == '__main__':
+    data = read_csv('50_states.csv')
+    states = data['state']
     screen = Screen()
     screen.title('U.S. States Quiz')
+    screen.setup(725, 491)
 
     image = 'blank_states_img.gif'
     screen.addshape(image)
+    shape(image)
 
-    turtle.shape(image)
-
+    print(states)
+    ans = screen.textinput(title='Guess the state', prompt='Enter the state\'s name')
     screen.exitonclick()
