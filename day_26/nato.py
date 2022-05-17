c8301f5f2838d0d906ca0349028ae32a86fd67b0
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-05-17 21:33:27 +0100

    working nato alphabet script

--- day_26/nato.py
+++ day_26/nato.py
@@ -3,4 +3,10 @@ import pandas as pd
 
 nato_alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
 
-print(nato_alphabet)
+nato_dict = dict(nato_alphabet.values)
+
+user_msg = str(input('Enter Your Message: '))
+nato_msg = [nato_dict[char.upper()] for char in user_msg]
+print(nato_msg)
+
+
