7bea44026bb1db729c5a2f89481180aab82544e2
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-05-18 18:57:28 +0100

    updated formatting

--- day_27/button.py
+++ day_27/button.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 from tkinter import Tk, Label, Button, Entry, StringVar
 
-window = Tk()
-window.title('I clicked a button!')
-window.minsize(width=500, height=800)
-
-text_label = Label(text="Click that button!", font=("Arial", 30, "normal"))
-text_label.pack()
-
-user_msg = StringVar()
-text_input = Entry(window, textvariable=user_msg, width=30)
-text_input.pack()
-
 
 def button_clicked():
     text_label["text"] = user_msg.get()
     user_msg.set('')
 
 
-btn = Button(text='Click me', command=button_clicked)
+window = Tk()
+window.title('I clicked a button!')
+window.minsize(width=500, height=800)
+
+text_label = Label(window, text="Click that button!", font=("Arial", 30, "normal"))
+user_msg = StringVar()
+text_input = Entry(window, textvariable=user_msg, width=30)
+btn = Button(window, text='Click me', command=button_clicked)
+
+text_label.pack()
+text_input.pack()
 btn.pack()
 
 window.mainloop()
