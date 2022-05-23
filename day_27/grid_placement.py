4e7a3234f1381ed3425c516a4a81507dd4968495
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-05-23 22:38:45 +0100

    placing elements using a grid layout

--- day_27/grid_placement.py
+++ day_27/grid_placement.py
@@ -1,5 +1,17 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Button, Entry
 
 window = Tk()
 window.title('Grid layout')
+
+label = Label(window, text='Lorem Ipsum', font=('Arial', 30, 'normal'))
+btn_1 = Button(window, text='Click me')
+btn_2 = Button(window, text='Don\'t click me')
+txt_input = Entry(window, width=50)
+
+label.grid(column=0, row=0)
+btn_1.grid(column=1, row=1)
+btn_2.grid(column=2, row=0)
+txt_input.grid(column=3, row=3)
+
+
 window.mainloop()
