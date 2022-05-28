9da4393356ab1ce1d8b959f12f5db3e756df8559
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2022-05-28 23:00:54 +0100

    initial solution for finding happy numbers

---
+++ day_28/happy_num.py
@@ -0,0 +1,14 @@
+def is_happy(n):
+    prev_num = []
+    happy = False
+
+    while not happy:
+        digits = [int(num) for num in str(n)]
+        sum = 0
+
+        for i in digits:
+            sum += i * i
+
+        print(i)
+
+is_happy(12345)
